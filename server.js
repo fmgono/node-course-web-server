635ec33124662de7f5224e5b87b8565591d4caf5
Fathan Margono <fmgonoo@gmail.com>
2018-05-27 13:38:00 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+});
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    // console.log(req);
+
+
+
+    fs.appendFile('requset.log', req, (err) => {
+        if (err) {
+            console.log('Unable to append server.log');
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Homepage',
+        welcomeMessage: 'Welcome to my homepage'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Me'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+});
+
+
+app.listen(3000, () => console.log('Server is start on port 3000'));
